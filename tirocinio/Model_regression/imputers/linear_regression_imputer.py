ee36e947089b424d48158ff78e2201b0ad34633d
MatteoBisotti <matteo.bisotti@studenti.unimi.it>
2024-05-23 09:46:32 +0200

    sistemata organizzazione file

--- tirocinio/Model_regression/imputers/linear_regression_imputer.py
+++
@@ -1,9 +0,0 @@
-from .imputer_regression import ImputerRegression
-from sklearn.linear_model import LinearRegression
-
-class LinearRegressionImputer(ImputerRegression):
-
-    def get_model(self):
-        
-        model = LinearRegression()
-        return model
--- tirocinio/Model_regression/imputers/logistic_regression_imputer.py
+++
@@ -1,9 +0,0 @@
-from sklearn.linear_model import LogisticRegression
-from .imputer_regression import ImputerRegression
-
-class LogisticRegressionImputer(ImputerRegression):
-
-    def get_model(self):
-
-        model = LogisticRegression()
-        return model
